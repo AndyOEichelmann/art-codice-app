242e459dea8519d13559aa03a5e662d87c2bd8a7
Andres Ortega Eichelmann <andyoeichelmann@gmail.com>
2023-07-24 16:48:29 -0500

    Finish Art Certificate of Authenticity token contract test

--- test/ACoA.js
+++ test/ACoA.js
@@ -20,7 +20,7 @@ describe("Test ACoA token contract", function () {
     describe("MintTokens", async function () {
         
         it("artist should mint token and tranfer it to caollector twise", async function () {
-            const { testACoA, artist, collector1, collector2 } = await loadFixture(deployTestACoAFixture);
+            const { testACoA, owner, artist, collector1, collector2 } = await loadFixture(deployTestACoAFixture);
 
             // grant roll
             const roleMint = await testACoA.MINTER_ROLE();
@@ -39,7 +39,10 @@ describe("Test ACoA token contract", function () {
             // transer certificate to a collector
             await expect (testACoA.connect(artist).safeTransferFromValue(artist.address, collector1.address, 0, 600)).to.emit(testACoA, 'NewValue');
 
-            await expect (testACoA.connect(collector1).safeTransferFromValue(collector1.address, collector2.address, 0, 600)).to.emit(testACoA, 'Transfer');
+            // grant operation approval to contract owner
+            await expect(testACoA.connect(collector1).setApprovalForAll(owner.address, true)).to.emit(testACoA, 'ApprovalForAll').withArgs(collector1.address, owner.address, true);
+
+            await expect (testACoA.safeTransferFromValue(collector1.address, collector2.address, 0, 600)).to.emit(testACoA, 'Transfer');
         });
 
         it("collector should fail to transfer and the value mus not have changed", async function () {
@@ -71,8 +74,45 @@ describe("Test ACoA token contract", function () {
             expect(tokenInfo.value).to.not.equal(newValue);
         });
 
-        it("should", async function () {
-            
+        it("should mint multiple tokens from different users", async function () {
+            const { testACoA, artist, collector1, collector2 } = await loadFixture(deployTestACoAFixture);
+
+            // grant roll
+            const roleMint = await testACoA.MINTER_ROLE();
+            expect(await testACoA.grantRole(roleMint, artist.address)).to.emit(testACoA, 'RoleGranted');
+
+            // mint certificate tokens
+            const toAddr = artist.address;
+            const currency = "usd";
+            const artistName = "Jhon Dowe"
+            let objectName = "Painting No"
+            let authURI = `${baseAURI}painting`
+            let value = 500;
+
+            const mintNumber = 5;
+
+            for(let i = 0; i < mintNumber ;i++){
+                await expect (testACoA.connect(artist).safeMint(
+                        toAddr, 
+                        value, 
+                        currency, 
+                        artistName, 
+                        `${objectName} ${i + 1}`, 
+                        `${authURI} ${i + 1}`)
+                    ).to.emit(testACoA, 'Minted').withArgs(i, artistName, `${objectName} ${i + 1}`);
+                
+                value += 250;
+            }
+
+            // minted count
+            const minteed = await testACoA.mintedTokens();
+            expect(minteed).to.equal(mintNumber);
+
+            // verify minted tokens
+            const firstToken = await testACoA.authnticateToken(0);
+            console.log(firstToken);
+            const lastToken = await testACoA.connect(artist).authnticateToken(mintNumber - 1);
+            console.log(lastToken);
         })        
     });
 });
