bf593f121d4322334b058a6e7c6c1e4aab103075
Andres Ortega Eichelmann <andyoeichelmann@gmail.com>
2023-07-26 16:58:41 -0500

    Complete the Intermediary contracts & tests
    Add requirements to listing Intem
    Create the claim function

--- contracts/ERC721ACoA_Intermediary.sol
+++ contracts/ERC721ACoA_Intermediary.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
+
 import "./IERC721ACoA.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract ERC721ACoA_Intermediary is ReentrancyGuard {
+contract ERC721ACoA_Intermediary is Context, ReentrancyGuard{
     using Counters for Counters.Counter;
 
 
     // state varaibles
     address payable public immutable feeAcount;
-        // change to listing fee
-    uint256 public immutable feePercent;
+    uint256 public immutable listingFee;
 
     Counters.Counter private _itemCounter;
 
@@ -20,43 +23,72 @@ contract ERC721ACoA_Intermediary is ReentrancyGuard {
         uint256 itemId;
         IERC721ACoA nftContract;
         uint256 tokenId;
+        uint64 objectValue;
         address lister;
         address claimer;
-        bool claimed;
+        bool isClaimed;
     }
 
     // itemId -> Item
     mapping(uint256 => Item) public _itemsListed;
 
     // constructor
-    constructor(uint256 _feePercent){
+    constructor(uint256 _listingFee){
         feeAcount = payable(msg.sender);
-        feePercent = _feePercent;
+        listingFee = _listingFee;
     }
 
-    event ClaimItemListed (uint256 itemId, address indexed nftContract, uint256 tokenId, address indexed lister, address indexed claimer);
+    event ItemListed (uint256 itemId, address indexed nftContract, uint256 tokenId, address indexed lister, address indexed claimer);
 
+    event ItemClaimed (uint256 itemId, address indexed nftContract, uint256 tokenId, address indexed lister, address indexed claimer, bool isClaimed);
 
-    // List transferablo token / certificate
-    function listCertificate(IERC721ACoA _nftContract, uint256 _tokenId, address _claimer) external nonReentrant {
-        // requirements checks ~ msg.sender is the item owner, this contract is approved for token | approve for all, claimer not addres cero, ...
-        // _nftContract.isApprovedForAll()
+    // List transferablo token / certificate ~ sender must pay listing fee
+    function listCertificate(IERC721ACoA _nftContract, uint256 _tokenId, uint64 _objectValue, address _claimer) external nonReentrant {
+        require(_nftContract.ownerOf(_tokenId) == _msgSender(), string(abi.encodePacked("Intermediary: account ", Strings.toHexString(_msgSender())," is not token owner")));
+
+        require(_nftContract.isApprovedForAll(_msgSender(), address(this)), string(abi.encodePacked("Intermediary: ", Strings.toHexString(address(this)), " contract must be appove by token owner")));
+        
+        // console.log("intermediary is approved for token",_nftContract.getApproved(_tokenId) == address(this));
 
         // obtain itemId & increment count
         uint256 _itemId = _itemCounter.current();
         _itemCounter.increment();
 
-        // ? transfer item to thise contract
-
         // add new item to listedItems mapping
-        _itemsListed[_itemId] = Item (_itemId, _nftContract, _tokenId, msg.sender, _claimer, false);
+        _itemsListed[_itemId] = Item (_itemId, _nftContract, _tokenId, _objectValue, _msgSender(), _claimer, false);
 
         // listed events
-        emit ClaimItemListed (_itemId, address(_nftContract), _tokenId, msg.sender, _claimer);
+        emit ItemListed (_itemId, address(_nftContract), _tokenId, _msgSender(), _claimer);
     }
+
+    
+
     // Claim certificate
+    function claimCertificate(uint256 _itemId) external nonReentrant {
+        uint256 listedAmount =  _itemCounter.current();
+        
+        require(_itemId >= 0 && _itemId <= listedAmount, string(abi.encodePacked("Intermediary: item ", Strings.toString(_itemId), " does not exist")));
+
+        Item storage item = _itemsListed[_itemId];
+
+        require(_msgSender() == item.claimer, string(abi.encodePacked("Intermediary: ", Strings.toHexString(_msgSender()), " is not item permited claimer")));
+        
+        require(!item.isClaimed, string(abi.encodePacked("Intermediary: item ", Strings.toString(_itemId), " has allready been claimed")));
+
+        // update item transfer status
+        item.isClaimed = true;
+
+        // transfer nft certificate to claimer in behaf of the ownwe
+        item.nftContract.safeTransferFromValue(item.lister, item.claimer, item.tokenId, item.objectValue);
+
+        // emit item claimed event
+        emit ItemClaimed(_itemId, address(item.nftContract), item.tokenId, item.lister, _msgSender(), true);
+    }
 
     // Cancel listed certificate
+    function cancellListing(uint256 _itemId) external nonReentrant {
+        
+    }
 
     // view number of items listed
     function listedItems() external view returns (uint256 listedAmount) {
--- test/ArtCodiceIntermediary.js
+++ test/ArtCodiceIntermediary.js
@@ -42,35 +42,35 @@ describe("Art Codice Intermediary", function () {
             
             // retrive intermediary contract deplyment info {feeAcount, listingFee}
             expect(await intermediary.feeAcount()).to.equal(deployer.address);
-            expect(await intermediary.feePercent()).to.equal(listingFee);
+            expect(await intermediary.listingFee()).to.equal(listingFee);
         });
     });
 
-    describe("Art Codice Intermediary", function () {
+    describe("Art Codice Intermediary listing", function () {
         async function mintTokensFixture() {
             const {intermediary, testACoA, account1, account2} = await loadFixture(deployContractsFixture);
 
             // grant roll
             const roleMint = await testACoA.MINTER_ROLE();
             expect(await testACoA.grantRole(roleMint, account1.address)).to.emit(testACoA, 'RoleGranted');
+            expect(await testACoA.grantRole(roleMint, account2.address)).to.emit(testACoA, 'RoleGranted');
 
             // account 1 mint ceertificate token
             await testACoA.connect(account1).safeMint(account1.address, value, currency, artist, `${object} 1`, `${baseAURI}${object}1`);
             // account 1 approves for all the intermediaryn contract
             await testACoA.connect(account1).setApprovalForAll(intermediary.target, true);
 
+            // account 2 mint ceertificate token
+            await testACoA.connect(account2).safeMint(account2.address, value, currency, artist, `${object} 2`, `${baseAURI}${object}2`);
+
             return{ intermediary, testACoA, account1, account2 };
         }
 
-        it("should list new item", async function () {
+        it("should list new item, emit a ItemListed event and track the listed item", async function () {
             const {intermediary, testACoA, account1, account2} = await loadFixture(mintTokensFixture);
 
-            console.log(`listing fee: ${listingFee}`);
-            // console.log('intermediary addres',intermediary.target);
-            // console.log('ACoA addres',testACoA.target);
-
             // account 1 list Certificate 1 to be climed by account 2
-            await expect(intermediary.connect(account1).listCertificate(testACoA.target, 0, account2.address)).to.emit(intermediary, "ClaimItemListed").withArgs(0, testACoA.target, 0, account1.address, account2.address);
+            await expect(intermediary.connect(account1).listCertificate(testACoA.target, 0, 600, account2.address)).to.emit(intermediary, "ItemListed").withArgs(0, testACoA.target, 0, account1.address, account2.address);
 
             // retrive item amount
             const itemAmount = await intermediary.listedItems();
@@ -83,7 +83,39 @@ describe("Art Codice Intermediary", function () {
             expect(item.tokenId).to.equal(0);
             expect(item.lister).to.equal(account1.address);
             expect(item.claimer).to.equal(account2.address);
-            expect(item.claimed).to.equal(false);
+            expect(item.isClaimed).to.equal(false);
         });
+
+        it("should fail to list item from an addres not owner of token and fail to list item for not aprove intermediary", async function () {
+            const {intermediary, testACoA, account1, account2} = await loadFixture(mintTokensFixture);
+
+            // fail list Certificate 0 to be climed by account 2
+            await expect(intermediary.listCertificate(testACoA.target, 0, 600, account2.address)).to.be.revertedWith(/Intermediary: account .* is not token owner/);
+
+            // fail list Certificate 1 to be climed by account 1
+            await expect(intermediary.connect(account2).listCertificate(testACoA.target, 1, 1000, account1.address)).to.be.revertedWith(/Intermediary: .* contract must be appove by token owner/);
+        });
+    });
+
+    describe("Art Codice Intermediary claim listed items", function () {
+        async function mintTokensFixture() {
+            const {intermediary, testACoA, account1, account2} = await loadFixture(deployContractsFixture);
+
+            // grant roll
+            const roleMint = await testACoA.MINTER_ROLE();
+            expect(await testACoA.grantRole(roleMint, account1.address)).to.emit(testACoA, 'RoleGranted');
+            expect(await testACoA.grantRole(roleMint, account2.address)).to.emit(testACoA, 'RoleGranted');
+
+            // account 1 mint ceertificate token
+            await testACoA.connect(account1).safeMint(account1.address, value, currency, artist, `${object} 1`, `${baseAURI}${object}1`);
+            // account 1 approves for all the intermediary contract
+            await testACoA.connect(account1).setApprovalForAll(intermediary.target, true);
+            // account 1 lists the token in th eintermwdiary contract
+
+            // account 2 mint ceertificate token
+            await testACoA.connect(account2).safeMint(account2.address, value, currency, artist, `${object} 2`, `${baseAURI}${object}2`);
+
+            return{ intermediary, testACoA, account1, account2 };
+        }
     });
 });
